7271c4be6546c444923b1f759f11cb58bb207e70
DavidPichardoToyo <dpichardot31@gmail.com>
2025-06-26 16:32:42 +0000

    avance tienda.py

--- tienda.py
+++ tienda.py
@@ -14,9 +14,30 @@ class Tienda:
     def delivery(self):
         return self._delivery
     
-    def ingresar_producto(self, producto, Producto):
+    def ingresar_producto(self, producto: Producto):
         for p in self._productos:
             if p == producto:
                 p + producto
                 return
         self._productos.append(producto)
+
+    def listar_producto(self):
+        raise NotImplementedError
+    
+    def realizar_venta(self):
+        raise NotImplementedError
+    
+    class Restaurante(Tienda):
+        def ingresar_producto(self, producto: Producto):
+            producto.stock = 0
+            super().ingresar_producto(producto)
+
+        def listar_producto(self):
+            result = f"Productos de Restaurante {self.nombre}:\n"
+            for p in self._productos:
+                result += f" - {p.nombre} | ${p.precio}\n"
+            
+            return result
+        
+        def realizar_venta(self,nombre_procuto: str, cantidad: int):
+            pass # No modifica stock
